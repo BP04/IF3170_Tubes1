ab5c1c4e19d2314888ee5604d263c846cae0d5aa
allen2610 <jessicaallen.lim@gmail.com>
2025-10-09 22:40:58 +0700

    feat: added Simulated Annealing Algorithm

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from utils import *
 from models import *
 from scheduler import *
+from simulated_annealing import simulated_annealing
 
 def main():
-    courses, rooms, students = load_data_from_json('data/input.json')
+    courses, rooms, students = load_data_from_json('../data/input.json')
 
     time_slots = [TimeSlot(day, hour) for day in ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'] for hour in range(8, 17)]
 
@@ -11,6 +12,22 @@ def main():
     initial_fitness = fitness(initial_schedule, students)
 
     print(f"Initial Schedule Fitness: {initial_fitness}")
+
+    print("\nPilih algoritma:")
+    print("1. Hill Climbing")
+    print("2. Simulated Annealing")
+    print("3. Genetic Algorithm")
+
+    choice = input("Masukkan nomor algoritma: ")
+
+    if choice == "1":
+        print("Hill Climbing belum diimplementasikan.")
+    elif choice == "2":
+        simulated_annealing(initial_schedule, students, time_slots, rooms)
+    elif choice == "3":
+        print("Genetic Algorithm belum diimplementasikan.")
+    else:
+        print("Pilihan tidak valid!")
 
 if __name__ == "__main__":
     main()
